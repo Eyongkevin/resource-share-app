dde9bd1ea9afa71620f08edc6c7a88fe6267abe7
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2023-09-21 15:29:42 +0100

    use pull_request_target

--- apps/resources/form.py
+++ apps/resources/form.py
@@ -4,7 +4,6 @@ from .models import Tag
 # TODO: Add fields for category(Radio) and tags(Select)
 
 # Testing this
-tags = [tag.name for tag in Tag.objects.all()]
 
 
 class PostResourceForm(forms.Form):
