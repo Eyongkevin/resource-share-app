2013998b19bf91a9bbadfdc24cd0f30d6e022129
Eyongkevin <tonyparkerkenz@gmail.com>
2024-03-03 17:19:51 +0100

    fix keyError in log middleware

--- apps/core/middleware/log.py
+++ apps/core/middleware/log.py
@@ -10,7 +10,7 @@ def simple_logging_middleware(get_response):
         http_method = request.method
         url = request.get_full_path()
         host_port = request.get_host()
-        content_type = request.headers["Content-Type"]
+        content_type = request.headers.get("Content-Type", "")
         user_agent = request.headers["User-Agent"]
 
         msg = f"{http_method} | {host_port}{url} | {content_type} | {user_agent}"
